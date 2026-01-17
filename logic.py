197d49644952c19baf65e0f841fa6c447627928d
Erasyl-02 <103504281+Erasyl-02@users.noreply.github.com>
2026-01-18 00:58:37 +0500

    Added commands list, update, mark-in-progress, mark-done

--- logic.py
+++ logic.py
@@ -22,8 +22,42 @@ def add_task(desc):
 
 def list_task():
         tasks = storage.load_data()
-        count = 0
-        for i in tasks:
-                print(i[count])
-                count += 1
 
+        if not tasks:
+                print("The list is empty")
+                return
+
+        for task in tasks:
+                id = task['id']
+                desc = task['description']
+                status = task['status']  
+                print(f"[{id}] {desc} — ({status})")
+
+def update_task(task_id, changes):
+        if not changes:
+                print('Type updates')
+                return
+        
+        tasks = storage.load_data()
+
+        for task in tasks:
+                if task['id'] == int(task_id):
+                        task['description'] = changes
+                        storage.save_data(tasks)
+                        print(f'Task with ID:{task_id} has been updated')
+                        return
+
+        print(f'Task with ID:{task_id} doesnt exist')
+
+def mark_task(task_id, change):
+        tasks = storage.load_data()
+
+        for task in tasks:
+                if task['id'] == int(task_id):
+                        task['status'] = change
+                        task['updatedAt'] = datetime.now().isoformat()
+                        storage.save_data(tasks)
+                        print(f'Task with ID:{task_id} marked as {change}')
+                        return
+        
+        print(f'Task with ID:{task_id} doesnt exist')
--- main.py
+++ main.py
@@ -4,35 +4,40 @@ from datetime import datetime
 import json
 import logic
 
-DB_File = 'database.json'
 
 #command = sys.argv[1]
 def main():
-    # if len(sys.argv) < 3:
-    #     print('type a command')
-    #     return
+    if len(sys.argv) < 3 and sys.argv[1] != 'list':
+         print('type a command')
+         return
     command = sys.argv[1]
-    command2 = sys.argv[2]
     if command == 'add':
+        command2 = sys.argv[2]
         logic.add_task(command2)
     if command == 'list':
         if len(sys.argv) < 3:
             logic.list_task()
+            return
         else:
+            command2 = sys.argv[2]
             if command2 == 'done':
                 print('this should show done tasks')
             if command2 == 'todo':
                 print('this should show task that should be done')
             if command2 == 'in-progress':
                 print('these are tasks that are in progress')
+    command2 = sys.argv[2]
     if command == 'delete':
         print('this is delete command')
     if command == 'update':
-        print('this is update command')
+        if len(sys.argv) < 4:
+            print('You should type changes')
+            return
+        logic.update_task(sys.argv[2], sys.argv[3])
     if command == 'mark-in-progress':
-        print('this is mark-in-progress command')
+        logic.mark_task(sys.argv[2], 'in-progress')
     if command == 'mark-done':
-        print('this is mark-done command')
+        logic.mark_task(sys.argv[2], 'done')
 
 if __name__ == "__main__":
     main()
